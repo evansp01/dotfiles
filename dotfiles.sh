950aff22496396d37d7781568e54489fba328e8d
Evan Palmer <evansp01@gmail.com>
2015-07-11 21:20:14 -0700

    added os support to script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,48 +1,114 @@
 #!/bin/bash
 
 backup_dir="."
+common_prefix="$backup_dir/common"
+osx_prefix="$backup_dir/osx"
+linux_prefix="$backup_dir/linux"
+verbose=1
 
 config_files=(
-    .zshrc
-    .zsh_aliases
     .vimrc
-    .bashrc
-    .bash_aliases
     .tmux.conf
     .editorconfig
 )
 
+osx_config_files=(
+    .zshrc
+    .zsh_aliases
+    .bashrc
+    .bash_aliases
+)
+
+linux_config_files=(
+    .zshrc
+    .zsh_aliases
+    .bashrc
+    .bash_aliases
+)
+
+usage(){
+    echo "./dotfiles.sh [option]"
+    echo "    backup - backup the curent config"
+    echo "    unpack - restore backed up config"
+}
+
+
+determine_platform() {
+    if [[ $(uname) == 'Linux' ]]; then
+        if (( verbose > 0 )); then
+            echo "Running on linux"
+        fi
+        os_prefix=$linux_prefix
+        os_config_files=(${linux_config_files[@]})
+    elif [[ $(uname) == 'Darwin' ]]; then
+        if (( verbose > 0 )); then
+            echo "Running on osx"
+        fi
+        os_prefix=$osx_prefix
+        os_config_files=(${osx_config_files[@]})
+    else
+        echo "Could not determine platform"
+        exit
+    fi
+}
+
+
+
 
 run_backup() {
+    file_dir=$1; shift
+    mkdir -p $file_dir
 
-    mkdir -p $backup_dir
-
-    for file in "${config_files[@]}"; do
+    for file in "$@"; do
+        if [ ! -f "$HOME/$file" ]; then
+            continue
+        fi
         save_name=$(echo "$file" | sed -E 's/^\.(.*)/\1/')
-        cp "$HOME/$file" "$backup_dir/$save_name"
+        if (( verbose > 0 )); then
+            echo "$HOME/$file -> $file_dir/$save_name"
+        fi
+        cp "$HOME/$file" "$file_dir/$save_name"
     done
 
 }
 
 run_unpack() {
+    file_dir=$1; shift
 
-    for file in "${config_files[@]}"; do
+    for file in "$@"; do
         save_name=$(echo "$file" | sed -E 's/^\.(.*)/\1/')
-        cp "$backup_dir/$save_name" "$HOME/$file"
+        if [ ! -f "$file_dir/$save_name" ]; then
+            continue
+        fi
+        if (( verbose > 0 )); then
+            echo "$file_dir/$save_name -> $HOME/$file"
+        fi
+        cp "$file_dir/$save_name" "$HOME/$file"
     done
 
 }
 
-case $1 in
+if (( $# < 1 )); then
+    usage
+    exit
+fi
 
+
+
+case $1 in
     backup)
-        run_backup
+        determine_platform
+        run_backup $common_prefix ${config_files[@]}
+        run_backup $os_prefix ${os_config_files[@]}
+
         ;;
     unpack)
-        run_unpack
+        determine_platform
+        run_unpack $common_prefix ${config_files[@]}
+        run_unpack $os_prefix ${os_config_files[@]}
         ;;
     *)
-        echo "unknown option"
+        usage
         exit
         ;;
 esac
