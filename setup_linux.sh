ade6bd84638623f9806422ed2c7af0150089bbda
Evan Palmer <evansp01@gmail.com>
2016-01-21 13:45:34 -0600

    a bunch of improvements to linux setup script

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,15 +1,24 @@
 # unpack defalt configurations
-./dotfiles unpack
+./dotfiles.sh unpack
 # install oh my zsh and zsh
-sudo apt-get install vim zsh python3-pip
+sudo apt-get install vim zsh python3-pip tmux vim-gnome
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ./extra/esp.zsh-theme ~/.oh-my-zsh/themes
 # install vundle and vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 # setup you complete me
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+#cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+# install syntastic linters
 sudo apt-get install npm
-sudo pip3 install flake8 
+sudo pip3 install flake8
 sudo npm install -g jshint
+# install autojump
+sudo apt-get install autojump
+# install patched font for vim airline
+wget https://github.com/abertsch/Menlo-for-Powerline/raw/master/Menlo%20for%20Powerline.ttf
+mkdir -p ~/.fonts
+mv "Menlo for Powerline.ttf" ~/.fonts
+fc-cache -vf ~/.fonts
